ba729d2d888d398216b3d31bfcb0734e600f8bb6
YeThura-424 <rathanpoudle.26@gmail.com>
2024-06-24 22:49:59 +0630

    bulk seed

--- database/seed.js
+++ database/seed.js
@@ -5,14 +5,18 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.VITE_SUPERBASE_URL, process.env.SERVICE_ROLE_KEY)
 
-const seedProjects = async () => {
-  const name = faker.lorem.word(3)
-  await supabase.from('projects').insert({
-    name: name,
-    slug: name.toLocaleLowerCase().replace(/ /g, '-'),
-    status: faker.helpers.arrayElement(['in-progress', 'completed']),
-    collabrators: faker.helpers.arrayElements([1, 2, 3, 4])
-  })
+const seedProjects = async (entry) => {
+  const projects = []
+  for (let i = 0; i < entry; i++) {
+    const name = faker.lorem.word(3)
+    projects.push({
+      name: name,
+      slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collabrators: faker.helpers.arrayElements([1, 2, 3, 4])
+    })
+  }
+  await supabase.from('projects').insert(projects)
 }
 
-await seedProjects()
+await seedProjects(10)
