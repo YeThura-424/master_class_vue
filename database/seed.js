a531b21eee3fe61a650fba521b52038f8002d74f
YeThura-424 <rathanpoudle.26@gmail.com>
2024-06-24 22:47:01 +0630

    seeder for project, command line

--- database/seed.js
+++ database/seed.js
@@ -5,4 +5,14 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.VITE_SUPERBASE_URL, process.env.SERVICE_ROLE_KEY)
 
-console.log(supabase)
+const seedProjects = async () => {
+  const name = faker.lorem.word(3)
+  await supabase.from('projects').insert({
+    name: name,
+    slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+    status: faker.helpers.arrayElement(['in-progress', 'completed']),
+    collabrators: faker.helpers.arrayElements([1, 2, 3, 4])
+  })
+}
+
+await seedProjects()
