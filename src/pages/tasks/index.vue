ae0e407352e9abc31ab0025c9cd0ac9082015efb
YeThura-424 <rathanpoudle.26@gmail.com>
2024-11-17 22:21:24 +0630

    tasks page and types for tasks table

---
+++ src/pages/tasks/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { supabase } from '@/lib/superbaseClient'
+import { ref } from 'vue'
+import type { Tables } from '../../../database/types'
+const tasks = ref<Tables<'tasks'>[] | null>(null)
+
+;(async () => {
+  const { data, error } = await supabase.from('tasks').select()
+
+  if (error) console.log(error)
+
+  tasks.value = data
+})()
+</script>
+
+<template>
+  <div>
+    <h1>Projects Page</h1>
+    <pre>{{ tasks }}</pre>
+    <RouterLink to="/">Home</RouterLink>
+  </div>
+</template>
