36613d27dd7be970e8860d4a175ff04fc4386757
YeThura-424 <rathanpoudle.26@gmail.com>
2024-11-28 21:58:47 +0630

    update: retreving auth user in router for each navigation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
-import { supabase } from './lib/superbaseClient'
-
 const errorStore = useErrorStore()
-const authStore = useAuthStore()
 onErrorCaptured((error) => {
   errorStore.setError({ error })
 })
 
-onMounted(async () => {
-  const { data } = await supabase.auth.getSession()
-
-  if (data.session?.user) await authStore.setAuth(data.session)
-})
+onMounted(async () => {})
 </script>
 
 <template>
