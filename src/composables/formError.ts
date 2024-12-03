d3584eb6619e2523206232599640a6476ae539fb
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-03 22:09:37 +0630

    update: form error composable

--- src/composables/formError.ts
+++ src/composables/formError.ts
@@ -1,15 +1,33 @@
+import type { Login } from '@/types/Auth'
 import type { AuthError } from '@supabase/supabase-js'
 
 export const useFormError = () => {
   const serverError = ref('')
+  const realTimeError = ref()
 
   const handelServerError = (error: AuthError) => {
     serverError.value =
       error.message === 'Invalid login credentials' ? 'Incorrect Email or Password' : error.message
   }
 
+  const handelLoginForm = async (formData: Login) => {
+    realTimeError.value = {
+      email: [],
+      password: []
+    }
+
+    const { validateEmail, validatePassword } = await import('@/utils/formValidations')
+
+    const emailErrors = validateEmail(formData.email)
+    if (emailErrors) realTimeError.value.email = emailErrors
+
+    const passwordErrors = validatePassword(formData.password)
+    if (passwordErrors) realTimeError.value.password = passwordErrors
+  }
+
   return {
     serverError,
-    handelServerError
+    handelServerError,
+    handelLoginForm
   }
 }
