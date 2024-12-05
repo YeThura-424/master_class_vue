142fc2722c943293a21741295c84954cd67a2284
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-05 22:29:22 +0630

    use vueuse useMemoize for caching

--- src/stores/loders/projects.ts
+++ src/stores/loders/projects.ts
@@ -1,13 +1,13 @@
 import { type projectsType } from '@/utils/supaQueries'
 import { projectsQuery } from '@/utils/supaQueries'
-
+import { useMemoize } from '@vueuse/core'
 export const useProjectStore = defineStore('projects-store', () => {
   const projects = ref<projectsType | null>(null)
-
+  const loadedProjects = useMemoize(async (key: string) => await projectsQuery)
   const fetchProject = async () => {
-    if (projects.value?.length) return
+    // if (projects.value?.length) return
 
-    const { data, error, status } = await projectsQuery
+    const { data, error, status } = await loadedProjects('projects')
 
     if (error) useErrorStore().setError({ error, customeCode: status })
 
