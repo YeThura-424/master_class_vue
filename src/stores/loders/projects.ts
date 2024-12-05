7e60ea75b45be78fd4c1bd004c10157ac1914f20
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-05 22:41:02 +0630

    update:for stale while revalidate

--- src/stores/loders/projects.ts
+++ src/stores/loders/projects.ts
@@ -3,9 +3,27 @@ import { projectsQuery } from '@/utils/supaQueries'
 import { useMemoize } from '@vueuse/core'
 export const useProjectStore = defineStore('projects-store', () => {
   const projects = ref<projectsType | null>(null)
+
   const loadedProjects = useMemoize(async (key: string) => await projectsQuery)
+
+  const validateCache = () => {
+    // logic for deleting cached data if something in database has changed
+    if (projects.value?.length) {
+      projectsQuery.then(({ data }) => {
+        if (JSON.stringify(projects.value) === JSON.stringify(data)) {
+          console.log('Cached and fresh data matched!')
+          return
+        } else {
+          console.log('Something has changed!!')
+          loadedProjects.delete('projects')
+        }
+      })
+    }
+  }
+
   const fetchProject = async () => {
     // if (projects.value?.length) return
+    validateCache()
 
     const { data, error, status } = await loadedProjects('projects')
 
