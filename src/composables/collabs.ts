b7730d638d1e40b78c22df9cb87219f8ab9352bc
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-09 21:40:41 +0630

    grouped profile from project

--- src/composables/collabs.ts
+++ src/composables/collabs.ts
@@ -1,6 +1,10 @@
+import type { GroupedCollabs } from '@/types/GroupedCollabs'
 import { projectCollabQuery } from '@/utils/supaQueries'
+import type { projectsType, tasksType } from '@/utils/supaQueries'
 
 export const useCollabs = () => {
+  const groupedCollabs = ref<GroupedCollabs>({})
+
   const getProfileByIds = async (projIds: string[]) => {
     const { data, error } = await projectCollabQuery(projIds)
 
@@ -9,7 +13,20 @@ export const useCollabs = () => {
     return data
   }
 
+  const getGroupedCollabs = async (items: projectsType | tasksType) => {
+    const filteredItems = items.filter((item) => item.collaborators.length)
+    const promises = filteredItems.map((item) => getProfileByIds(item.collaborators))
+
+    const results = await Promise.all(promises)
+
+    filteredItems.forEach((item, index) => {
+      groupedCollabs.value[item.id] = results[index]
+    })
+  }
+
   return {
-    getProfileByIds
+    getProfileByIds,
+    getGroupedCollabs,
+    groupedCollabs
   }
 }
