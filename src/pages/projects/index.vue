43d83fe71ea6626cf8a417f4521d04e4401deb75
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-10 22:04:39 +0630

    update projects index

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -2,22 +2,20 @@
 import { columns } from '@/utils/tableColumns/projectsColumns'
 
 usePageStore().pageData.title = 'Projects'
-const projectsLoder = useProjectStore()
-const { projects } = storeToRefs(projectsLoder)
-const { fetchProject } = projectsLoder
 
-await fetchProject()
+const projectsLoader = useProjectStore()
+const { projects } = storeToRefs(projectsLoader)
+const { getProjects } = projectsLoader
 
-// const { getProfileByIds } = useCollabs()
+await getProjects()
 
-// const test = getProfileByIds(projects.value[0].collaborators)
 const { getGroupedCollabs, groupedCollabs } = useCollabs()
 
-await getGroupedCollabs(projects.value)
+getGroupedCollabs(projects.value)
 
-console.log(groupedCollabs, 'test data')
+const columnsWithCollabs = columns(groupedCollabs)
 </script>
 
 <template>
-  <DataTable v-if="projects" :columns="columns" :data="projects" />
+  <DataTable v-if="projects" :columns="columnsWithCollabs" :data="projects" />
 </template>
