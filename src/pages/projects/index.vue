5694d8ebb922f09cb8bd3d71bafbc9a71d9a1c41
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-12 19:12:00 +0630

    update:projects listing page

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -11,7 +11,7 @@ await fetchProject()
 
 const { getGroupedCollabs, groupedCollabs } = useCollabs()
 
-getGroupedCollabs(projects.value)
+getGroupedCollabs(projects.value ?? [])
 
 const columnsWithCollabs = columns(groupedCollabs)
 </script>
