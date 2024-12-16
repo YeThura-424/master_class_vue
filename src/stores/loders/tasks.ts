ecac68a4cef817d1463db1f1366ed7dbcf2612ea
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-16 21:44:15 +0630

    add:cache validation in tasks loader

--- src/stores/loders/tasks.ts
+++ src/stores/loders/tasks.ts
@@ -1,11 +1,45 @@
 import type { tasksType, taskType } from '@/utils/supaQueries'
 import { taskQuery, tasksQuery } from '@/utils/supaQueries'
+import { useMemoize } from '@vueuse/core'
 
 export const useTaskStore = defineStore('task-store', () => {
   const tasks = ref<tasksType | null>(null)
   const singleTask = ref<taskType | null>(null)
 
+  const loadedTasks = useMemoize(async (key: string) => await tasksQuery)
+  const loadedSingleTask = useMemoize(async (slug: string) => await taskQuery(slug))
+
+  interface ValidateCacheParams {
+    ref: typeof tasks | typeof singleTask
+    query: typeof tasksQuery | typeof taskQuery
+    key: string
+    loader: typeof loadedTasks | typeof loadedSingleTask
+  }
+
+  const validateCache = ({ ref, query, key, loader }: ValidateCacheParams) => {
+    const finalQuery = typeof query === 'function' ? query(key) : query
+
+    if (ref.value) {
+      finalQuery.then(({ data, error }) => {
+        if (JSON.stringify(ref.value) === JSON.stringify(data)) {
+          return
+        } else {
+          loader.delete(key)
+          if (!error && data) {
+            ref.value = data
+          }
+        }
+      })
+    }
+  }
+
   const fetchTasks = async () => {
+    validateCache({
+      ref: tasks,
+      query: tasksQuery,
+      key: 'tasks',
+      loader: loadedTasks
+    })
     const { data, error, status } = await tasksQuery
 
     if (error) useErrorStore().setError({ error, customeCode: status })
@@ -14,6 +48,13 @@ export const useTaskStore = defineStore('task-store', () => {
   }
 
   const fetchSingleTask = async (id: string) => {
+    validateCache({
+      ref: singleTask,
+      query: taskQuery,
+      key: id,
+      loader: loadedSingleTask
+    })
+
     const { data, error, status } = await taskQuery(id)
 
     if (error) useErrorStore().setError({ error, customeCode: status })
