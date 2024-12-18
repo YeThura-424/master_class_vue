3f0609d2a8ccd405d916f3771fa068ff4d68edab
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-18 21:53:55 +0630

    create:AppNewTask component

---
+++ src/components/AppNew/AppNewTask.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Sheet>
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Are you absolutely sure?</SheetTitle>
+        <SheetDescription>
+          This action cannot be undone. This will permanently delete your account and remove your
+          data from our servers.
+        </SheetDescription>
+      </SheetHeader>
+    </SheetContent>
+  </Sheet>
+</template>
