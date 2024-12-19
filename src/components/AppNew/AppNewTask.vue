27959250b756a6bb162b29b2a4471059b7db864a
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-19 21:38:35 +0630

    add:new task form

--- src/components/AppNew/AppNewTask.vue
+++ src/components/AppNew/AppNewTask.vue
@@ -6,10 +6,33 @@ const sheetOpen = defineModel()
   <Sheet v-model:open="sheetOpen">
     <SheetContent>
       <SheetHeader>
-        <SheetTitle>Are you absolutely sure?</SheetTitle>
-        <p>Contents</p>
-
-        <FormKit type="text" />
+        <SheetTitle>Create New Task</SheetTitle>
+        <FormKit type="form" @submit="console.log('here')" submit-label="Create">
+          <FormKit type="text" name="name" id="name" label="Name" placeholder="My new task" />
+          <FormKit
+            type="select"
+            name="for"
+            id="for"
+            label="For"
+            placeholder="Select a user"
+            :options="[{ label: 'Item name', value: 1 }]"
+          />
+          <FormKit
+            type="textarea"
+            name="description"
+            id="description"
+            label="Description"
+            placeholder="Task description"
+          />
+          <FormKit
+            type="select"
+            name="project"
+            id="project"
+            label="Porject"
+            placeholder="Select a project"
+            :options="[{ label: 'Item name', value: 1 }]"
+          />
+        </FormKit>
       </SheetHeader>
     </SheetContent>
   </Sheet>
