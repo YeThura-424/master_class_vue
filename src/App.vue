b2b8507fe0e62d0c034f531d550b99032eb9201c
YeThura-424 <rathanpoudle.26@gmail.com>
2024-12-22 21:06:32 +0630

    add:transaction on page change

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,20 @@ const { user } = storeToRefs(useAuthStore())
   <Component :is="user ? AuthLayout : GuestLayout">
     <AppErrorPage v-if="errorStore.activeError" />
     <RouterView v-else v-slot="{ Component, route }">
-      <Suspense v-if="Component" :timeout="0">
-        <Component :is="Component" :key="route.name"></Component>
-        <template #fallback>
-          <span>Loading...</span>
-        </template>
-      </Suspense>
+      <Transaction name="fade" mode="out-in">
+        <div class="w-full" :key="route.path">
+          <Suspense v-if="Component" :timeout="0">
+            <Component :is="Component"></Component>
+            <template #fallback>
+              <div
+                class="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 flex justify-center items-center w-full h-screen bg-background bg-opacity-90 z-50"
+              >
+                <iconify-icon icon="lucide:loader-circle" class="text-6xl animate-spin" />
+              </div>
+            </template>
+          </Suspense>
+        </div>
+      </Transaction>
     </RouterView>
   </Component>
 </template>
